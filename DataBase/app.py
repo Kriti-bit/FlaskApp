f3ef72dd83e0e51b66c1d457c1922f534e312b8d
iamkriti <69788174+Kriti-bit@users.noreply.github.com>
2022-07-05 19:26:55 +0530

    added display route

--- DataBase/app.py
+++ DataBase/app.py
@@ -56,13 +56,17 @@ def register():
 
 @app.route("/index")
 def index():
-    if 'loggedin' in session:
-        return render_template("index.html")
-    return redirect(url_for('login'))
+    pass
 
 
 @app.route("/display")
 def display():
+    if 'loggedin' in session:
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE id = % s',
+                       (session['id'], ))
+        account = cursor.fetchone()
+        return render_template("display.html", account=account)
     return redirect(url_for('login'))
 
 
