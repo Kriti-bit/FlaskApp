037a938c6bbc31f74087402e37a428c2575e2093
iamkriti <69788174+Kriti-bit@users.noreply.github.com>
2022-07-06 15:46:39 +0530

    added SQL Query

---
+++ DataBase_Query.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE IF NOT EXISTS `DataBase` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `DataBase`;
+CREATE TABLE IF NOT EXISTS `accounts`(
+`id` int(11) NOT NULL AUTO_INCREMENT,
+`username` varchar(58) NOT NULL,
+`password` varchar(255) NOT NULL,
+`email` varchar(100) NOT NULL,
+`organisation` varchar(100) NOT NULL,
+`address` varchar(100) NOT NULL,
+`city` varchar(100) NOT NULL,
+`state` varchar(160) NOT NULL,
+`country` varchar(100) NOT NULL,
+`postalcode` varchar (100) NOT NULL,
+PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
